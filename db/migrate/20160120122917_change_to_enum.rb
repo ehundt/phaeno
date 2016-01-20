e3aa89ec74dacc7626354d0b5ca2ff5faeca9b94
Elisabeth Hundt <elisabeth.hoelscher@gmail.com>
2016-01-20 13:35:42 +0100

    one file too much committed in the last commit

--- db/migrate/20160120122917_change_to_enum.rb
+++ db/migrate/20160120122917_change_to_enum.rb
@@ -1,4 +1,7 @@
 class ChangeToEnum < ActiveRecord::Migration
   def change
+    # rename_column :phaenological_seasons, :season_id, :season
+    # rename_column :season_indications, :season_id, :season
+    # drop
   end
 end
